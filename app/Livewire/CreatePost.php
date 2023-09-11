fd85f2d797e4b3e9efc89567602e78c22436516d
Luis Parrado <luisprmat@gmail.com>
2023-09-11 09:26:37 -0500

    Form validation using new PHP Attributes

--- app/Livewire/CreatePost.php
+++ app/Livewire/CreatePost.php
@@ -3,12 +3,15 @@
 namespace App\Livewire;
 
 use App\Models\Post;
+use Livewire\Attributes\Rule;
 use Livewire\Component;
 
 class CreatePost extends Component
 {
+    #[Rule(['required', 'min:5'])]
     public string $title = '';
 
+    #[Rule(['required', 'min:5'])]
     public string $body = '';
 
     public function render()
@@ -18,6 +21,8 @@ class CreatePost extends Component
 
     public function save(): void
     {
+        $this->validate();
+
         Post::create([
             'title' => $this->title,
             'body' => $this->body,
